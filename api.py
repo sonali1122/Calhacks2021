937c990f84e7b0724ff3793e494782ad3d8a2df9
sonali1122 <70306424+sonali1122@users.noreply.github.com>
2021-10-23 19:51:04 -0700

    Added stuff, unfinished

--- api.py
+++ api.py
@@ -9,6 +9,45 @@ token = "9830cb8f6d90acc4"
 
 endpoint = 'https://api.matterport.com/api/models/graph'
 
+hazards = {
+	"Electrical outlets": ["HIGH RISK"
+	"Exposed wires": 
+	"Stairs": ["HIGH RISK"
+	"Appliances": 
+	"Bathrooms": 
+	"Chemicals": 
+	"Doors": 
+	"Furniture": "LOW RISK"
+	"Fireplaces": []
+	"Small objects": ["HIGH RISK"
+	"Sharp objects": 
+	"Recalled Item":
+	"Windows":
+}
+
+def risk_assign(tagLabel, tagID, position){
+	if tagLabel == "Appliances":
+		if (position.z > ):
+	elif tagLabel == "Chemicals":
+	
+	elif tagLabel == "Small objects":
+	
+	elif tagLabel == "Sharp objects":
+	
+	elif tagLabel == "Windows":
+	
+	else:
+		return #the specific hazard's risk
+	
+		
+}
+
+def mitigations(tagLabel, tagID, position){
+	if (tagLabel == 
+	
+}
+
+
 query = {
 	"query": '''
 		query{
@@ -17,6 +56,7 @@ query = {
 					id
 					label
 					description
+					position
 				}
 			}
 
@@ -33,4 +73,12 @@ print("DATA LEN:")
 print(len(r.json()['data']['model']['mattertags']))
 print("DATA:")
 print(r.json()['data']['model']['mattertags'])
+
+mutation{
+	patchMattertag(
+		#change tag color
+		#in title, add risk level in brackets
+		#in description, add mitigations
+	
+}
 
